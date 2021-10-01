685491ae3ccbb2134999a0dc5e37a63e95825046
Bryant Wells <mail@bryantwells.com>
2021-10-01 19:45:55 -0400

    final final

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
-const expandableElements = document.querySelectorAll('.u-expandable');
-const prompts = document.querySelectorAll('.Prompt');
-const promptContainers = document.querySelectorAll('.Prompt-container');
+const metaElements = document.querySelectorAll('.Meta');
 
-expandableElements.forEach((expandableElement) => {
-    const expandableHeader = expandableElement.querySelector('.u-expandableHeader');
-    // expandableHeader.addEventListener('mouseleave', () => {
-    //     prompts.forEach((prompt) => {
-    //         prompt.classList.remove('is-active');
-    //     });
-    // });
-    // expandableHeader.addEventListener('mouseenter', () => {
-    //     prompts.forEach((prompt) => {
-    //         prompt.classList.add('is-active');
-    //     });
-    // });
-    expandableHeader.addEventListener('click', () => {
-        expandableElement.classList.toggle('is-expanded');
+metaElements.forEach((metaElement) => {
+    const metaHeader = metaElement.querySelector('.Meta-header');
+    metaHeader.addEventListener('click', () => {
+        metaElement.classList.toggle('is-expanded');
     });
 });
-
-promptContainers.forEach((promptContainer) => {
-    promptContainer.addEventListener('mouseleave', () => {
-        prompts.forEach((prompt) => {
-            prompt.classList.remove('is-active');
-        });
-    });
-    promptContainer.addEventListener('mouseenter', () => {
-        prompts.forEach((prompt) => {
-            prompt.classList.add('is-active');
-        });
-    });
-})
