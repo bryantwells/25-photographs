dfdbad111b3ac3db668bac178f8b1089df3aa74f
Bryant Wells <mail@bryantwells.com>
2021-11-12 15:12:02 -0500

    style submission page

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,16 @@ metaElements.forEach((metaElement) => {
         metaElement.classList.toggle('is-expanded');
     });
 });
+
+const submissions = [...document.querySelectorAll('.Submission')];
+
+submissions.forEach((submission, i) => {
+    submission.addEventListener('click', () => {
+        if (submission.classList.contains('is-active')) {
+            submission.classList.remove('is-active');
+        } else {
+            submissions.forEach((sibling) => { sibling.classList.remove('is-active') });
+            submission.classList.add('is-active');
+        }
+    })
+})
--- site/templates/collection.php
+++ site/templates/collection.php
@@ -1,19 +1,29 @@
 <?php snippet('header') ?>
 <style>
-    /* .CollectionTitle {
-        grid-column: <?= ceil($page->num() % 5) + 1 ?> / <?= ceil($page->num() % 5) + 2 ?>;
-        grid-row: <?= ceil($page->num() / 5) + 1 ?> / <?= ceil($page->num() / 5) + 2 ?>;
-    } */
+    .CollectionTitle {
+        grid-column: 1 / 2;
+        grid-row: 1 / 2;
+    }
 </style>
 <main>
-    <h1 class="CollectionTitle"><?= $page->num() ?><br><?= $page->title() ?></h1>
+    <nav class="Nav"><a href="/" class="Nav-link">Back</a></nav>
     <?php $submissions = $page->files() ?>
     <?php foreach ($submissions as $submission): ?>
-        <article class="Submission">
-            <figure class="Submission-figure">
-                <img class="Submission-image" src="<?= $submission->url() ?>" title="Photo by <?= $submission->attribution() ?>">
-                <figcaption class="Submission-caption"><?= $submission->attribution() ?></figcaption>
-            </figure>
+        <article class="Submission" data-hash="<?= $submission->hash() ?>">
+                <style>
+                    [data-hash='<?= $submission->hash() ?>'].is-active {
+                        grid-row-start: <?= ceil(($submission->indexOf() + 2) / 5) ?>;
+                    }
+                    @media screen and (max-width: 1000px) {
+                        [data-hash='<?= $submission->hash() ?>'].is-active {
+                            grid-row-start: unset;
+                        }
+                    }
+                </style>
+                <figure class="Submission-figure">
+                    <img class="Submission-image" srcset="<?= $submission->srcset([300, 600, 1200]) ?>" src="<?= $submission->resize(600)->url() ?>" title="Photo by <?= $submission->attribution() ?>">
+                    <figcaption class="Submission-caption"><?= $submission->attribution() ?></figcaption>
+                </figure>
         </article>
     <?php endforeach ?>
 </main>
